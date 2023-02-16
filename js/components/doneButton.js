2712e27d1bf58368f7a6d1996318c6651bbffcec
César MORO <cesar.moro.dev@gmail.com>
2023-02-16 17:42:19 +0100

    JS : composant bouton 'tâche terminée' : récupération du bouton cliqué et de sa tâche parente RÉUSSI via .currentTarget et .parentNode

--- js/components/doneButton.js
+++ js/components/doneButton.js
@@ -2,23 +2,38 @@ export function doneButtonFunction() {
         console.log("doneButton.JS >> doneButtonFunction chargée");
 
                 //* Récupération des éléments utiles
+                // Tous les boutons "tâche exécutée"
                 let tableauDesBoutonsTacheExecutee = document.querySelectorAll('.executee');
+                // La section contenant les tâches à exécuter
+                let sectionDesTachesAExecuter = document.querySelector('#main__left .all-tasks');
+                // La sectoin contenant les tâches déjà exécutées
+                let sectionDesTachesTerminees = document.querySelector('#main__right .all-tasks');
+                console.log(sectionDesTachesTerminees);
+
 
 
                 //* Je crée des éléments utiles
 
 
                 //* Fonctions de traitement pour les événements déclenchés
-                function validerLaTache(paramTacheCiblee) {
-                        console.log(paramTacheCiblee)
-                        
+                function marquerLaTacheCommeExecutee(paramBoutonDeTacheExecutee) {
+                        /**
+                         * NOTE : puisque l'argument retourné est un objet, on doit l'exploiter comme tel.
+                         * Ce qui signifie atteindre sa propriété "currentTarget" pour le manipuler
+                         */
+
+                        // Je récupère la tâche associée au bouton cliqué (troisième parent du bouton)
+                        let tacheExecuteeParenteDuBoutonClique  = paramBoutonDeTacheExecutee.currentTarget.parentNode.parentNode.parentNode;
+                        // console.log(tacheExecuteeParenteDuBoutonClique);
+
+
                 }
+                
 
                 //* Je crée un événement
-                tableauDesBoutonsTacheExecutee.forEach(element => {
-                        // Pour chaque bouton de tâche exécutée, je récupère la tâche associée (quatrième parent)
-                        let tacheCibleeParLeBouton = element.parentNode.parentNode.parentNode.parentNode;
-                        element. addEventListener('click', validerLaTache, tacheCibleeParLeBouton);
+                tableauDesBoutonsTacheExecutee.forEach(boutonDeLaTacheExecutee => {
+                        // NOTE : l'élément retourné est un objet. Il faudra l'exploiter comme tel dans la fonction de traitement
+                        boutonDeLaTacheExecutee. addEventListener('click', marquerLaTacheCommeExecutee, boutonDeLaTacheExecutee);
                 })
                 
 
