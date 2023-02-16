5ce82bf0a925b9a36679ae5eb27a8dd7aeadad6b
César MORO <cesar.moro.dev@gmail.com>
2023-02-16 18:09:27 +0100

    Déplacement d'une tâche d e 'à exécuter' vers 'terminée' : RÉUS SIE. Je dois encore comprendre pourqu oi elle se supprime automatiquement d 'une section alors que je n'ai codé q u'une copie de la div (querySelector puis appendChild). + Masquage de la d escription lorsque la tâche bascule e n tâche terminée

--- js/components/doneButton.js
+++ js/components/doneButton.js
@@ -6,9 +6,6 @@ export function doneButtonFunction() {
                 let tableauDesBoutonsTacheExecutee = document.querySelectorAll('.executee');
                 // La section contenant les tâches à exécuter
                 let sectionDesTachesAExecuter = document.querySelector('#main__left .all-tasks');
-                // La sectoin contenant les tâches déjà exécutées
-                let sectionDesTachesTerminees = document.querySelector('#main__right .all-tasks');
-                console.log(sectionDesTachesTerminees);
 
 
 
@@ -24,7 +21,31 @@ export function doneButtonFunction() {
 
                         // Je récupère la tâche associée au bouton cliqué (troisième parent du bouton)
                         let tacheExecuteeParenteDuBoutonClique  = paramBoutonDeTacheExecutee.currentTarget.parentNode.parentNode.parentNode;
-                        // console.log(tacheExecuteeParenteDuBoutonClique);
+                        
+                        /**
+                         * > Je veux déplacer cette tâche de la section "à exécuter" vers la section "terminées"
+                         * Je commence par créer une nouvelle div dans la section "terminées" pour y inclure la tâche et son contenu
+                         * 
+                         * Ensuite, une fois dans la section "terminées", je devrai :
+                         *      -> Cacher la description de la tâche
+                         *      -> Changer les icônes
+                         */
+
+                        //> 1 - Je crée la nouvelle div dans la section "terminées" pour inclure ma tâche terminée 
+                        // Je crée une nouvelle div de tâche terminée (donc sans oublier les class nécessaires !)
+                        let nouvelleDivDeTacheTerminee = document.createElement('div');
+                        // J'inclus dans cette div ma tache récupérée via l'argument dans l'event Listener
+                        nouvelleDivDeTacheTerminee.appendChild(tacheExecuteeParenteDuBoutonClique);
+                        // Maintenant, j'inclus cette div dans la section des "tâches terminées"
+                        // Je récupère la section contenant les tâches déjà exécutées
+                        let sectionDesTachesTerminees = document.querySelector('#main__right .all-tasks');
+                        sectionDesTachesTerminees.appendChild(nouvelleDivDeTacheTerminee);
+                        //! Ça fonctionne dès ce point, mais pourquoi ça supprime la div de la section "à exécuter" alors que je suis censé l'avoir dupliquée ? (voir appendChild)
+
+                        //> 2 - J'ajoute la classe pour masquer la description
+                        // Je récupère la description
+                        let descriptionDeLaTache = nouvelleDivDeTacheTerminee.querySelector('.description');
+                        descriptionDeLaTache.classList.add('display-none');
 
 
                 }
