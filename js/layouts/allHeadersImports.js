190d81fe82bbf2f0c933a6bcebd1633486f8d0f4
César MORO <cesar.moro.dev@gmail.com>
2023-02-18 13:46:05 +0100

    Construction de la fonction HEADER pour le calcul automatique des nombres de tâches et statistiques. Il faut également appeler cette fonction dans chaque composnt bouton qui a un impacte sur ces calculs

--- js/layouts/allHeadersImports.js
+++ js/layouts/allHeadersImports.js
@@ -1,8 +1,10 @@
 //> Import des différents modules de header
 import { ajouterUneNouvelletache } from "./header/addNewTask.js";
+import { statistiquesDesTachesDansLeHeader } from "./header/statisticsTasks.js";
 
 export function allHeaderCategories() {
         //console.log("header.JS >> headerFunction chargée");
 
         ajouterUneNouvelletache();
+        statistiquesDesTachesDansLeHeader();
 }
---
+++ js/layouts/header/statisticsTasks.js
@@ -0,0 +1,39 @@
+export function statistiquesDesTachesDansLeHeader() {
+        console.log('statisticsTasks.js >> statistiquesDesTachesDansLeHeader.js chargée');
+
+        //* Récupération des éléments utiles
+        // Emplacement du nombre total de tâches existantes
+        let emplacementNombreTotalDeTaches = document.getElementById('total-tasks');
+        // Emplacement du nombre de tâches terminées
+        let emplacementNombreDeTachesTerminees = document.getElementById('done-tasks');
+        // Emplacement de la stat du nombre de taches terminées =
+        let emplacementStatTachesTerminees = document.getElementById('stat-done-tasks');
+        // console.log(emplacementNombreTotalDeTaches, emplacementNombreDeTachesTerminees, emplacementStatTachesTerminees)
+
+        // Récupération du tableau de tâches à exécuter
+        let tableauDesTachesAExecuter = document.querySelectorAll('.one-task__to-do');
+        // Récupération du tableau de tâches déjà terminées
+        let tableauDesTachesTerminees = document.querySelectorAll('.one-task__done');
+        // console.log(tableauDesTachesAExecuter, tableauDesTachesTerminees);
+
+
+        //* Calculs
+        // nombre de tâches dans le tableau des tâches à exécuter
+        let nombreDeTachesAExecuter = tableauDesTachesAExecuter.length;
+        // nombre de tâches dans le tableau des tâches terminées
+        let nombreDeTachesTerminees = tableauDesTachesTerminees.length;
+        // nombre total de tâches existantes
+        let nombreTotalDeTachesExistantes = nombreDeTachesAExecuter + nombreDeTachesTerminees;
+        // statistique du nombre de tâches terminées arrondie à l'entier le plus proche
+        let statNombreDeTachesTerminees = Math.round((nombreDeTachesTerminees / nombreTotalDeTachesExistantes) *100);
+        //console.log(nombreDeTachesAExecuter, nombreDeTachesTerminees, nombreTotalDeTachesExistantes, statNombreDeTachesTerminees+'%');
+
+
+        //* Injection des calculs dans la page html
+        // Nombre de tâches total existantes
+        emplacementNombreTotalDeTaches.innerText = nombreTotalDeTachesExistantes;
+        // Nombre de tâches terminées
+        emplacementNombreDeTachesTerminees.textContent = nombreDeTachesTerminees;
+        // Stat du nombre de tâches terminées
+        emplacementStatTachesTerminees.textContent = statNombreDeTachesTerminees + ' %';
+}
